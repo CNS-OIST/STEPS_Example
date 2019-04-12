c3c12090179869144eaf2ced15b8d464845a2b81
Tristan Carel <tristan.carel@epfl.ch>
2019-04-12 10:28:06 +0200

    Fix and improve the script python_scripts/runall.sh
    * go back in proper directory even a test fails
    * behavior can now be controled with a few environment variables
    * add usage instructions in the script

--- python_scripts/runall.sh
+++ python_scripts/runall.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
-python -c "import steps; steps._greet()"
+
+# Usage:
+#
+# Run all tests: ./runall.sh
+# Use another python executable: PYTHON=/path/to/another/python ./runall.sh
+# Use another mpirun executable: MPIRUN=srun ./runall.sh
+# Skip tests that require X: NO_X=1 ./run/all.sh
+
+MPIRUN=${MPIRUN:-mpirun}
+PYTHON=${PYTHON:-python}
+
+${PYTHON} -c "import steps; steps._greet()"
 echo "Example: Well Mixed"
-cd well_mixed && python well_mixed.py && cd ..
+(cd well_mixed && ${PYTHON} well_mixed.py)
+
 echo "Example: IP3"
-cd ip3 && python ip3.py && cd ..
+(cd ip3 && ${PYTHON} ip3.py)
+
 echo "Example: Diffusion"
-cd diffusion && python diffusion.py && cd ..
+(cd diffusion && ${PYTHON} diffusion.py)
+
 echo "Example: Surface Diffusion"
-cd surface_diffusion && python surface_diffusion.py && python surface_diffusion_tetode.py && cd ..
+(cd surface_diffusion && ${PYTHON} surface_diffusion.py && ${PYTHON} surface_diffusion_tetode.py)
+
 echo "Example: Diffusion Boundary"
-cd diffusion_boundary && python diffusion_boundary.py && cd ..
+(cd diffusion_boundary && ${PYTHON} diffusion_boundary.py)
+
 echo "Example: Surface Diffusion Boundary"
-cd surface_diffusion_boundary && python surface_diffusion_boundary.py && mpirun -n 4 python surface_diffusion_boundary_parallel.py && cd ..
+(cd surface_diffusion_boundary && ${PYTHON} surface_diffusion_boundary.py && ${MPIRUN} -n 4 ${PYTHON} surface_diffusion_boundary_parallel.py)
+
 echo "Example: SBML"
-cd sbml && python sbml_import.py && cd ..
+(cd sbml && ${PYTHON} sbml_import.py)
+
 echo "Example: EField"
-cd HH_APprop && python -c "import runHH_APprop; runHH_APprop.plotVz(10); runHH_APprop.plotVz(20); runHH_APprop.plotVz(30); runHH_APprop.show();  runHH_APprop.plotIz(10); runHH_APprop.plotIz(20); runHH_APprop.plotIz(30); runHH_APprop.show()" && mpirun -n 4 python HH_APprop_tetopsplit.py && python -c "import runHH_APprop_tetode; runHH_APprop_tetode.plotVz(100); runHH_APprop_tetode.plotVz(200); runHH_APprop_tetode.plotVz(300); runHH_APprop_tetode.show()" && cd ..
+(cd HH_APprop && ${PYTHON} -c "import runHH_APprop; runHH_APprop.plotVz(10); runHH_APprop.plotVz(20); runHH_APprop.plotVz(30); runHH_APprop.show();  runHH_APprop.plotIz(10); runHH_APprop.plotIz(20); runHH_APprop.plotIz(30); runHH_APprop.show()" && ${MPIRUN} -n 4 ${PYTHON} HH_APprop_tetopsplit.py && ${PYTHON} -c "import runHH_APprop_tetode; runHH_APprop_tetode.plotVz(100); runHH_APprop_tetode.plotVz(200); runHH_APprop_tetode.plotVz(300); runHH_APprop_tetode.show()")
+
 echo "Example: Parallelization, expect longer runtime"
-cd parallel && python ssa_simulation.py && mpirun -n 5 python parallel_simulation.py && mpirun -n 5 python HH_APprop_tetopsplit.py && cd ..
+(cd parallel && ${PYTHON} ssa_simulation.py && ${MPIRUN} -n 5 ${PYTHON} parallel_simulation.py && ${MPIRUN} -n 5 ${PYTHON} HH_APprop_tetopsplit.py)
+
 echo "Example: Visualization Toolkit"
-cd visual && python ip3r_sim.py && cd ..
+if [ "x${NO_X}" = x ] ;then
+  cd visual && ${PYTHON} ip3r_sim.py)
+fi
